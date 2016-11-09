4bfbd958a935d3af227f20761cb892b8c573c9c3
Justin <harwoodjp@g.cofc.edu>
2016-11-09 13:27:31 -0500

    flexbox is easier?

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-$(document).ready(function(){	
+$(document).ready(function(){
+	$(".text p").click(function() {
+		$(this).replaceWith('<textarea>' + this.innerHTML + '</textarea>')
+		$("textarea").css("resize","none");
+		$("textarea").css("height","60%");
+	});
 });
